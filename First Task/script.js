b5a9d8d6d12c0483faae12d67209837715cf0c1c
gorinich <gorinich@academ.org>
2020-05-15 20:02:01 +0700

    second

--- First Task/script.js
+++ First Task/script.js
@@ -9,13 +9,13 @@ let appData = {
     timeData : time,
     savings : false
 };    
-    
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a2 = prompt("Во сколько обойдется?", ''),
-	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a4 = prompt("Во сколько обойдется?", '');
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
+
+for (let index = 0; index < 2; index++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    b = prompt("Во сколько обойдется?", '');
+    
+    appData.expenses[a] = b;
+}
 
 alert(appData.budget/30);
